4ce552fd2a9e28ee8fe525c2e2ec9ef1c861a0a8
ayzsw <121678677+ayzsw@users.noreply.github.com>
2023-05-09 18:43:20 +0600

    Add files via upload

---
+++ homeWokr_8Montn_3/Constrollers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeWokr_8Montn_3
+//
+//  Created by Аяз on 9/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ homeWokr_8Montn_3/Constrollers/structFood.swift
@@ -0,0 +1,19 @@
+//
+//  structFood.swift
+//  homeWokr_8Montn_3
+//
+//  Created by Аяз on 9/5/23.
+//
+
+import Foundation
+struct Foods {
+    var foodImg1: String
+    var labelBigNameFood1: String
+    var labelTime1: String
+    var labelOtcenka1: String
+    var labelCountry1: String
+    var labelTypeFood1: String
+    var labelDelivery1: String
+    var labelPrice1: String
+    var labelKM1: String
+}
